0ce433279f5199aa89640d3d73a49f7eaefbe4dc
Houston Green <greenhouston@gmail.com>
2022-07-25 08:00:10 -0500

    wip

--- src/views/landingPage/welcomeHeader.tsx
+++ src/views/landingPage/welcomeHeader.tsx
@@ -1,8 +1,35 @@
+import { url } from 'inspector';
+import Image from 'next/image';
+import hero from '../../../public/images/landing-page/hero-2xl.jpeg';
+import { useWindowSize } from '../../common/hooks';
+
 export function WelcomeHeader() {
+  const { width } = useWindowSize();
+
   return (
-    <header className="h-screen">
-      <h1>Let&apos;s Build Together</h1>
-      <p>I mostly build websites, but Legos are cool too.</p>
-    </header>
+    <div className="grid gap-x-6 gap-y-16 lg:h-screen lg:grid-cols-2">
+      <header className="container pt-30 sm:pt-60">
+        <h1 className="mb-14">
+          Let&apos;s Build
+          <br />
+          Together
+        </h1>
+        <p className="text-2xl md:text-3xl">
+          I mostly build websites, but Legos are cool too.
+        </p>
+      </header>
+      {/* {width < 640 ? ( */}
+      <Image alt="Lego Hero Image" src={hero} />
+      {/* ) : (
+        <div
+          className="h-full"
+          style={{
+            backgroundImage: "url('/images/landing-page/hero-2xl.jpeg')",
+            backgroundRepeat: 'no-repeat',
+            backgroundSize: 'cover',
+          }}
+        />
+      )} */}
+    </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -19,8 +19,19 @@ module.exports = {
       fontFamily: {
         'cormorant-garamond': 'Cormorant Garamond, serif',
       },
+      fontSize: {
+        h1: ['60px', '68px'],
+        'h1-sm': ['64px', '72px'],
+        'h1-md': ['76px', '76px'],
+        'h1-lg': ['88px', '88px'],
+        'h1-xl': ['96px', '96px'],
+        '2xl': ['24px', '36px'],
+        '3xl': ['28px', '40px'],
+        '4xl': ['36px', '48px'],
+      },
+      spacing: { 30: '7.5rem', 60: '15rem' },
     },
-    container: { center: true, padding: { DEFAULT: '1rem' } },
+    container: { center: true, padding: { DEFAULT: '1rem', lg: '2rem' } },
   },
   plugins: [],
 };
