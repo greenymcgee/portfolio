3be3a73aa3cde91a4c4248c6ca2434bb84c08031
greenymcgee <greenhouston@gmail.com>
2026-05-01 06:33:38 -0600

    Removes GET posts Route

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -1,53 +1,8 @@
 import { CreatePostDto } from '@/features/posts/dto/create-post.dto'
-import { FindAndCountPostsDto } from '@/features/posts/dto/find-and-count-posts.dto'
 import { PostService } from '@/features/posts/post.service'
 import { INTERNAL_SERVER_ERROR } from '@/globals/constants'
 import { createResponse } from '@/lib/db'
 import { logger } from '@/lib/logger'
-
-export async function GET(request: Request) {
-  const result = await PostService.findAndCount(
-    new FindAndCountPostsDto(
-      Object.fromEntries(new URL(request.url).searchParams),
-    ),
-  )
-  return result.match(
-    (response) => {
-      return createResponse({
-        body: { posts: response.posts, totalPages: response.totalPages },
-        status: response.status,
-        url: request.url,
-      })
-    },
-    (error) => {
-      switch (error.type) {
-        case 'dto':
-          return createResponse({
-            body: { type: error.type },
-            status: error.status,
-            url: request.url,
-          })
-        case 'entity': {
-          return createResponse({
-            body: { type: error.type },
-            status: error.status,
-            url: request.url,
-          })
-        }
-        default: {
-          logger.error(
-            { error: error satisfies never },
-            'UNHANDLED_GET_POSTS_ERROR',
-          )
-          return createResponse({
-            status: INTERNAL_SERVER_ERROR,
-            url: request.url,
-          })
-        }
-      }
-    },
-  )
-}
 
 export async function POST(request: Request) {
   const result = await PostService.create(new CreatePostDto(request))
